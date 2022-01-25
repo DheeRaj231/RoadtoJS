be4e7adddfaed8ca9e75989bef4afa6701358a67
Dheeraj752 <dheeraj7411610616@gmail.com>
2022-01-25 09:31:01 +0530

    new

--- readme.md
+++ readme.md
@@ -1,4 +1,3 @@
-
 # Day 1
 
 - Data Types
@@ -6,7 +5,7 @@
   - string
   - nan
   - undefined
-  - null 
+  - null
   - symbol
   - bigInt
 
@@ -15,11 +14,13 @@
     JS is Dynamic Typing lang where the interpreter assigns variables a type at runtime based on the variable's value at the time.__
 
 ### Variable Declarations
+
 - let
-- var 
+- var
 - const
 
 ## Operators:
+
 - Assignment operators
 - typeof operator
 - comparison operator
@@ -29,44 +30,51 @@
 
 - [Click on this link](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence)
 
-----------------------------------------------------
+---
 
 # Day 2
 
 ## Template literals:
+
     (*) Template literals can contain placeholders.    These  are indicated by the dollar sign and curly braces (${expression}). The expressions in the placeholders and the text between the backticks (` `) get passed to a function.
 
      NOTE: This is used for multi line strings.
 
-
 ## Conditional Statements(if/else)
+
     declaration of the variable shouldbe done outside the statements
     Else it shows reference error
 
 ## Type conversion
+
     Explicit conversion which is done by programmer
     Eg: string"" to Number
 
 ## Type Coerction:
+
     Implicit conversion which is automatic
     Eg:(-) converts string to number
        (+) converts input data  to string.
 
 ## falsy values:
+
      0,'',undefined,null,NaN
      used in conditional sattements
 
 ## Eqality Operators
+
     Strict Equality ===
     Loose Equality ==
 
 ## Difference Operator
+
      Strict !==
      Loose !=
 
 - prompt
 
-## Boolean Logic 
+## Boolean Logic
+
     ________________________________________
     AND(&&) | ALL THE VALUES SHOULD BE TRUE
     _________________________________________
@@ -74,14 +82,13 @@
     ________________________________________________
     NOT(!)  | REVERSE THE VALUE
     ___________________________________________________
-  
 
-
-----------------------------------------------------
+---
 
 # Day 3
 
 ## Switch Statements
+
         switch (variable) {
             case 'varname':
                   //body;
@@ -91,46 +98,50 @@
         }
 
 ## ->Statement and Expression
+
     (*)expression produces value.
     (*)statement which does not produce value.
 
 ## Ternary operator
+
     condition ? if condition is true : if condition is false
 
     Note: Template String can be used inside ternary operators
 
 ## Strict Mode
+
     Js enables strict mode to avoid bugs.
     Eg: reference error, reserved words such as interface,private...
 
-
-----------------------------------------------------
+---
 
 # Day 4
 
 ## FUNCTIONS
 
-
 ### FUNCTION DECLARATION:
-    Normal functions which are widely used 
+
+    Normal functions which are widely used
     In this hoisting is supported and use of this operator is allowed
 
-
 ### FUNCTION EXPRESSION
+
     The variable is declared to store the returned value of function
     Hoisting is not supported
 
 ### ARROW FUNCTION
-    This returns the value implicitly 
+
+    This returns the value implicitly
     It does not support this keyword
 
 - Functions can call another inside it with specific function name and parameters.
 
-----------------------------------------------------
+---
 
 # Day 5
 
 ## ARRAYS
+
      An array is a special variable, which can hold more than one value.Here the order of element matters i.e every element has index value based on their position in array.
      The base of index is 0.
 
@@ -138,23 +149,24 @@
      (*) Nesting of array is possible.
 
 - Adding element
-    - PUSH
-    - unshift
+
+  - PUSH
+  - unshift
 
 - Removing element
-    - POP
-    - shift
+
+  - POP
+  - shift
 
 - includes : checks if particular element is present in the array and returns boolean value as result.
 
-----------------------------------------------------
-
-# Day 5
+# Day 6
 
 ## OBJECTS
+
 - Retriving data using dot and Bracket Notation
 - Object Methods
-  - function inside objects. 
+  - function inside objects.
 - This keyword
 - Control Structure
   - for loop,while loop
@@ -162,3 +174,18 @@
 - Filling empty array
 - Looping Backwards
 - Nesting of loops
+
+---
+
+# Day 7
+
+- Developer Skills
+- Prettier
+- Configure Prettier
+- Configure Snippet(vs code)
+- use of Live-server, Node.js
+- Learn how to code(Don't be John)
+- Use Stack Overflow, google, MDN
+- console (warn, error, table)
+- Think Like a developer
+- Debugger
--- script.js
+++ script.js
@@ -387,87 +387,93 @@ const total = [bill[0] + tips[0], bill[1] + tips[1], bill[2] + tips[2]];
 
 console.log(tips, bill, total);
 */
-
+/*
 // DAY - 6 #FFF
 
 // OBJECTS
 
 const cars = {
-    carName: 'audi',
-    model: 2006,
-    speed: 360,
-    otherCars: ['bmw', 'benz', 'volvo'],
-    // calcModel: function (model) {
-    //     return 2022 - model;
-    // }
-    calcModel: function () {
-        this.modelAge = 2022 - this.model;
-        return this.modelAge;
-    },
+  carName: "audi",
+  model: 2006,
+  speed: 360,
+  otherCars: ["bmw", "benz", "volvo"],
+  // calcModel: function (model) {
+  //     return 2022 - model;
+  // }
+  calcModel: function () {
+    this.modelAge = 2022 - this.model;
+    return this.modelAge;
+  },
 
-    getSummary: function () {
-        return `${this.carName} is a ${this.calcModel()} old car company`
-    },
-
-
+  getSummary: function () {
+    return `${this.carName} is a ${this.calcModel()} old car company`;
+  },
 };
 
 console.log(cars.getSummary());
 console.log(cars.calcModel());
 
 console.log(cars.speed); //DOT OPERATOR
-console.log(cars['carName']);
+console.log(cars["carName"]);
 
-const interstedObj = prompt('which property of cars u wanna know');
+const interstedObj = prompt("which property of cars u wanna know");
 console.log(cars[interstedObj]);
 
-cars['newObj'] = 'new object added';
-console.log(cars)
+cars["newObj"] = "new object added";
+console.log(cars);
 
-console.log(cars.carName + " has " + cars.otherCars.length + " sub cars and better one is  " + cars.otherCars[0]);
+console.log(
+  cars.carName +
+    " has " +
+    cars.otherCars.length +
+    " sub cars and better one is  " +
+    cars.otherCars[0]
+);
 
 // CHALLENGE
 
 const mark = {
-    name: 'Mark',
-    mass: 68,
-    height: 1.69,
-    calcBMI: function () {
-        this.bMi = (this.mass) / (this.height * this.height);
-        return this.bMi;
-    }
-}
+  name: "Mark",
+  mass: 68,
+  height: 1.69,
+  calcBMI: function () {
+    this.bMi = this.mass / (this.height * this.height);
+    return this.bMi;
+  },
+};
 console.log(mark.calcBMI());
 
 const john = {
-    name: 'john',
-    mass: 92,
-    height: 1.92,
-    calcBMI: function () {
-        this.bMi2 = (this.mass) / (this.height * this.height);
-        return this.bMi2;
-    }
-}
+  name: "john",
+  mass: 92,
+  height: 1.92,
+  calcBMI: function () {
+    this.bMi2 = this.mass / (this.height * this.height);
+    return this.bMi2;
+  },
+};
 console.log(john.calcBMI());
-(mark.calcBMI() < john.calcBMI()) ? console.log(`${mark.name}'s BMI is ${mark.calcBMI()} higher than john`) : console.log(`${john.name}'s BMI is higher than mark`);
+mark.calcBMI() < john.calcBMI()
+  ? console.log(`${mark.name}'s BMI is ${mark.calcBMI()} higher than john`)
+  : console.log(`${john.name}'s BMI is higher than mark`);
 
 // FOR LOOP
 
 const Cars1 = {
-    carName: 'audi',
-    model: 2006,
-    speed: 360,
-    otherCars: ['bmw', 'benz', 'volvo'],
-}
+  carName: "audi",
+  model: 2006,
+  speed: 360,
+  otherCars: ["bmw", "benz", "volvo"],
+};
 console.log(Cars1.length);
 
 const empty = []; // empty array
 for (let i = 0; i <= 10; i++) {
-    console.log(Cars1[i]); //accessing elements of array
+  console.log(Cars1[i]); //accessing elements of array
 
-    //filling array
-    //empty[i] = typeof cars[i];
-    empty.push(typeof Cars1[i]);
+  //filling array
+  //empty[i] = typeof cars[i];
+  empty.push(typeof Cars1[i]);
 }
 
 // shifting array from one to another
@@ -475,47 +481,210 @@ const yearnew = [1991, 2001, 2007, 2020];
 const ag = [];
 
 for (let i = 0; i < yearnew.length; i++) {
-    ag.push(yearnew[i]);
+  ag.push(yearnew[i]);
 }
 console.log(ag);
 
 // Nesting of Loop
 
 for (let i = 1; i < 4; i++) {
-    console.log(`-----main loop  ${i} `);
+  console.log(`-----main loop  ${i} `);
 
-    for (let j = 1; j < 6; j++) {
-        console.log(`sub loop ${j}`);
-    }
+  for (let j = 1; j < 6; j++) {
+    console.log(`sub loop ${j}`);
+  }
 }
 
 // while loop
-let rep = 1
+let rep = 1;
 while (rep <= 10) {
-    console.log(`wow ${rep}`);
-    rep++;
+  console.log(`wow ${rep}`);
+  rep++;
 }
 
 // CHALLENGE
 
 const calcTip = function (bill) {
-    if (bill >= 50 && bill <= 300) {
-        return (bill * 0.15);
-    } else {
-        return (bill * 0.20);
-    }
-}
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.2;
+  }
+};
 
 const bill = [12, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tip = [];
 const total = [];
 
 for (i = 0; i < bill.length; i++) {
-    tip.push(calcTip(bill[i]));
-    total.push(bill[i] + tip[i]);
+  tip.push(calcTip(bill[i]));
+  total.push(bill[i] + tip[i]);
 }
 
 // const tips = [calcTip(bill[0]), calcTip(bill[1]), calcTip(bill[2])];
 // const total = [bill[0] + tips[0], bill[1] + tips[1], bill[2] + tips[2]];
 
 console.log(bill, tip, total);
+*/
+
+// DAY - 7 ###fff
+
+("use strict");
+
+/*
+///////////////////////////////////////
+// Using Google, StackOverflow and MDN
+
+// PROBLEM 1:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem
+// - What is temp amplitude? Answer: difference between highest and lowest temp
+// - How to compute max and min temperatures?
+// - What's a sensor error? And what do do?
+
+// 2) Breaking up into sub-problems
+// - How to ignore errors?
+// - Find max value in temp array
+// - Find min value in temp array
+// - Subtract min from max (amplitude) and return it
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+// PROBLEM 2:
+// Function should now receive 2 arrays of temps
+
+// 1) Understanding the problem
+// - With 2 arrays, should we implement functionality twice? NO! Just merge two arrays
+
+// 2) Breaking up into sub-problems
+// - Merge 2 arrays
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
+
+
+///////////////////////////////////////
+// Debugging with the Console and Breakpoints
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+
+    // C) FIX
+    // value: Number(prompt('Degrees celsius:')),
+    value: 10,
+  };
+
+  // B) FIND
+  console.table(measurement);
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) IDENTIFY
+console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENTIFY
+console.log(amplitudeBug);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+/*
+// 1) Understanding the problem
+// - Array transformed to string, separated by ...
+// - What is the X days? Answer: index + 1
+
+// 2) Breaking up into sub-problems
+// - Transform array into string
+// - Transform each element to string with ºC
+// - Strings needs to contain day (index + 1)
+// - Add ... between elements and start and end of string
+// - Log string to console
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
+
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  console.log('...' + str);
+};
+printForecast(data1);
+*/
